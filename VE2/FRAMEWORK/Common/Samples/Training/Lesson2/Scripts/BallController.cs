a9fed2aa40787bc9ea10e0d5d6092996c87a470d
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-07-07 13:03:22 +0100

    Remove clashing training folder

--- VE2/FRAMEWORK/Common/Samples/Training/Lesson2/Scripts/BallController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class BallController : MonoBehaviour
-{
-    public void SetupBall()
-    {
-        GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
-    }
-}
